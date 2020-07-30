09e6b8a69821b1226813bd0be11bd61c2d64ffb6
minhpham <minhpham0902@gmail.com>
2020-07-30 14:18:14 +0700

    Updated cli.py

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ Command-line interface for speaker recognition system
 import os
 import argparse
 from logzero import logger
+import tempfile
 import uuid
 import speech_recognition as sr
 
@@ -19,9 +20,7 @@ if __name__ == "__main__":
         help="Whether to overwrite the model"
     )
     parser.add_argument(
-        "--audio_data_dir",
-        default="./tmp",
-        help="Directoty to save audio data"
+        "--num_samples", type=int, default=3, help="Number of sound samples"
     )
     parser.add_argument(
         "--model_path",
@@ -34,11 +33,11 @@ if __name__ == "__main__":
         help='Task to do. Either "enroll" or "predict"')
     args = parser.parse_args()
     
-    os.makedirs(args.audio_data_dir, exist_ok=True)
     if args.task == "predict" and not os.path.isfile(args.model_path):
         raise ValueError("Please provide valid model path")
 
     r = sr.Recognizer()
+    r.pause_threshold = 1.0
     if args.task == "enroll":
         if os.path.isfile(args.model_path) and not args.overwrite_model:
             model = ModelInterface.load(args.model_path)
@@ -47,32 +46,33 @@ if __name__ == "__main__":
         print("***** Enroll sound data for one speaker *****")
         name = input("Enter your name: ")
         name = name.strip()
-        print(f"Hello {name}. Please input your voice 3 times")
-        i = 1
-        while i <= 3:
-            with sr.Microphone() as source:
-                audio = r.listen(source)
-            # Generate random filename
-            filename = os.path.join(args.audio_data_dir, name + "_" + str(uuid.uuid1()) + ".wav")
-            with open(filename, "wb") as file:
-                file.write(audio.get_wav_data())
-            # enroll a file
-            fs, signal = read_wav(filename)
-            model.enroll(name, fs, signal)
-            logger.info("wav file %s has been enrolled" % (filename))
-            i += 1
+        print(f"Hello {name}. Please input your voice {args.num_samples} times")
+        with tempfile.TemporaryDirectory() as tempdir:
+            i = 1
+            while i <= args.num_samples:
+                with sr.Microphone() as source:
+                    audio = r.listen(source)
+                # Generate random filename
+                filename = os.path.join(tempdir, name + "_" + str(uuid.uuid1()) + ".wav")
+                with open(filename, "wb") as file:
+                    file.write(audio.get_wav_data(convert_rate=16000))
+                # enroll a file
+                fs, signal = read_wav(filename)
+                model.enroll(name, fs, signal)
+                logger.info("wav file %s has been enrolled" % (filename))
+                i += 1
             
         model.train()
         model.dump(args.model_path)
     else:
         model = ModelInterface.load(args.model_path)
         print("Please input your voice: ")
-        with sr.Microphone() as source:
-            audio = r.listen(source)
-        filename = os.path.join(str(uuid.uuid1()) + ".wav")
-        with open(filename, "wb") as file:
-            file.write(audio.get_wav_data())
-        fs, signal = read_wav(filename)
-        pred, _ = model.predict(fs, signal)
-        print(f"Your name is {pred}!")
-    
+        with tempfile.TemporaryDirectory() as tempdir:
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+            filename = os.path.join(tempdir, str(uuid.uuid1()) + ".wav")
+            with open(filename, "wb") as file:
+                file.write(audio.get_wav_data(convert_rate=16000))
+            fs, signal = read_wav(filename)
+            pred, _ = model.predict(fs, signal)
+            print(f"Your name is {pred}!")
